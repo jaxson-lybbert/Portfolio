a2bcb4c7a59df3cb4fcc0c6e85ce4aada26f816e
jaxson-lybbert <jax.a.lybb@gmail.com>
2023-08-04 19:16:18 -0400

    resume.js

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -2,21 +2,21 @@ import resumeSnapshot from "../assets/Resume-Snapshot.png";
 import resumePDF from "../assets/Jaxson Lybbert Resume.pdf";
 
 export default function Resume() {
-  // Function will execute on click of button
-  const onButtonClick = () => {
-    // using Java Script method to get PDF file
-    fetch(resumePDF).then((response) => {
-      response.blob().then((blob) => {
-        // Creating new object of PDF file
-        const fileURL = window.URL.createObjectURL(blob);
-        // Setting various property values
-        let alink = document.createElement("a");
-        alink.href = fileURL;
-        alink.download = resumePDF;
-        alink.click();
-      });
-    });
-  };
+  // // Function will execute on click of button
+  // const onButtonClick = () => {
+  //   // using Java Script method to get PDF file
+  //   fetch(resumePDF).then((response) => {
+  //     response.blob().then((blob) => {
+  //       // Creating new object of PDF file
+  //       const fileURL = window.URL.createObjectURL(blob);
+  //       // Setting various property values
+  //       let alink = document.createElement("a");
+  //       alink.href = fileURL;
+  //       alink.download = resumePDF;
+  //       alink.click();
+  //     });
+  //   });
+  // };
   return (
     <>
       <h2>You can download a copy of my resume here.</h2>
